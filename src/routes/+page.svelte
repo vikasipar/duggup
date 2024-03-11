b9d3823420b98475fc39585d6e798beafeb6cc70
vikasipar <vickyipar01@gmail.com>
2024-03-11 19:08:31 +0530

    Made webpage mobile responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,94 +18,112 @@
 </svelte:head>	
 
 <div class="w-[98vw] mx-auto">
-<section class="w-[80%] mx-auto mt-9">
+<section class="w-[90%] lg:w-[80%] 2xl:w-[70%] mx-auto mt-9 lg:mt-16">
 	<div class="flex flex-row flex-wrap justify-between items-center">
-		<div class="w-[14%]">
-			<img src={profile} alt="user profile" class="w-fit">
-			<h1 class="text-2xl font-bold text-[#4B5C6D] text-center">Krishna Kiran</h1>
+		<div class="w-[30%] sm:w-[14%]">
+			<img src={profile} alt="user profile" class="w-[70%] mx-auto sm:w-fit">
+			<h1 class="text-sm sm:text-base md:text-2xl font-semibold md:font-bold text-[#4B5C6D] text-center">Krishna Kiran</h1>
 		</div>
-		<div class="w-[60%] px-11 text-sm">
-			<p class="">Co-Founder and CEO at Duggup - Social network for people in tech. Ex-Amazon Head of Engineering. I write hot-takes on building a business, shipping delightful products and accelerating product and career growth.</p>
-			<button class="mt-7 w-[91px] h-[36px] px-4 rounded-[4px] border border-b-[4px] border-[#4D4D4D] flex items-center justify-center outline-none gap-2 text-sm hover:cursor-pointer">Followed</button>
-		</div>
-		<div class="w-[14%] text-right flex flex-col justify-between">
+		<div class="w-[40%] sm:w-[14%] text-right flex sm:hidden flex-col justify-between">
 			<div class="space-y-[1px]">
-				<img src={company} alt="company icon" class="w-fit mr-0 ml-auto">
-				<h2 class="text-base font-bold">Duggup</h2>
-				<h4 class="text-xs text-[#7A9299]">Co-Founder and CEO</h4>
+				<img src={company} alt="company icon" class="w-[20%] sm:w-[25%] md:w-fit mr-0 ml-auto">
+				<h2 class="text-xs sm:text-sm md:text-base font-semibold md:font-bold">Duggup</h2>
+				<h4 class="text-[9px] md:text-xs text-[#7A9299]">Co-Founder and CEO</h4>
 			</div>
 			<div class="mt-3 flex items-center space-x-4">
-				<a href="www.duggup.com" target="_blank" class="text-sm flex gap-1 items-center">My website <img src={visit} alt="visit my website option" class="w-fit"></a>
-				<img src={more} alt="more options" class="w-fit hover:cursor-pointer">
+				<a href="https://duggup.com" target="_blank" class="text-[9px] md:text-sm flex gap-1 items-center">My website <img src={visit} alt="visit my website option" class="w-[11%] sm:w-[20%] md:w-fit"></a>
+				<img src={more} alt="more options" class="w-[12%] sm:w-[20%] md:w-fit hover:cursor-pointer">
+			</div>
+		</div>
+		<div class="w-full sm:w-[70%] mt-2 sm:mt-0 text-justify lg:text-left lg:w-[60%] px-3 sm:px-11 text-xs md:text-sm">
+			<p class="">Co-Founder and CEO at Duggup - Social network for people in tech. Ex-Amazon Head of Engineering. I write hot-takes on building a business, shipping delightful products and accelerating product and career growth.</p>
+			<button class="mt-5 md:mt-7 md:w-[91px] w-[70px] h-[25px] md:h-[36px] px-2 md:px-4 rounded-[4px] border border-b-[4px] border-[#4D4D4D] flex items-center justify-center outline-none gap-2 text-xs md:text-sm hover:cursor-pointer">Followed</button>
+		</div>
+		<div class="hidden w-[14%] text-right sm:flex flex-col justify-between">
+			<div class="space-y-[1px]">
+				<img src={company} alt="company icon" class="w-[25%] md:w-fit mr-0 ml-auto">
+				<h2 class="text-xs sm:text-sm md:text-base font-semibold md:font-bold">Duggup</h2>
+				<h4 class="text-[9px] md:text-xs text-[#7A9299]">Co-Founder and CEO</h4>
+			</div>
+			<div class="mt-3 flex items-center space-x-4">
+				<a href="https://duggup.com" target="_blank" class="text-[9px] md:text-sm flex gap-1 items-center">My website <img src={visit} alt="visit my website option" class="w-[20%] md:w-fit"></a>
+				<img src={more} alt="more options" class="w-[20%] md:w-fit hover:cursor-pointer">
 			</div>
 		</div>
 	</div>	
 </section>
 
 
-<div class="w-[80%] mx-auto text-[14px] my-0">
-	<div class="flex flex-row flex-wrap items-center gap-11 ">
+<div class="w-[80%] 2xl:w-[70%] mx-auto text-[14px] my-0">
+	<div class="flex flex-row items-center gap-12">
 		<div class="w-fit flex items-center">
 			<p class="text-xs text-[#7A9299]">Dec 2023</p>
 			<img src={progress} alt="progress line" class="w-fit">
 		</div>
-		<div class="w-[344px] border rounded-lg">
-			<img src={stevej} alt="steve jobs' thumbnail" class="w-fit">
-			<p class="px-8 py-4">No amount of technology can convert a bad story into a good story.</p>
-		</div>
-		<div class="w-[344px] border rounded-lg">
-			<img src={sama} alt="sam altman's thumbnail" class="w-fit rounded-t-xl">
-			<p class="px-8 py-4">Most people don't have original ideas. Here is how Sam Altman pushes himself to have...</p>
-		</div>
-	</div>
-</div>
-
-<div class="w-[92%] my-0 mx-auto">
-	<div class="flex gap-2 flex-row flex-wrap items-center">
-		<div class="flex flex-col w-fit gap-1 text-right">
-			<img src={company} alt="company logo" class="w-9 ml-auto">
-			<h3 class="text-base font-bold">Duggup</h3>
-			<span class="text-xs text-[#7A9299]">San Francisco Bay Area</span>
-			<span class="text-xs text-[#7A9299]">Joined Nov 2023</span>
-		</div>
-		<img src={forward} alt="forward line" class="w-6">
-		<div class="text-xs text-[#7A9299] w-fit ml-14">
-			<h3 class="text-xl font-bold">Co-Founder and CEO</h3>
-			<h5 class="text-xs text-[#7A9299]">Full-time · Remote</h5>
+		<div class="flex flex-wrap w-fit justify-center mx-auto mt-4 sm:mt-9 gap-2 sm:gap-4 xl:gap-9">
+			<div class="w-[344px] border rounded-lg">
+				<img src={stevej} alt="steve jobs' thumbnail" class="w-fit">
+				<p class="px-1 sm:px-8 py-1 sm:py-4 text-xs md:text-sm">No amount of technology can convert a bad story into a good story.</p>
+			</div>
+			<div class="w-[344px] border rounded-lg">
+				<img src={sama} alt="sam altman's thumbnail" class="w-fit rounded-t-xl">
+				<p class="px-1 sm:px-8 py-1 sm:py-4 text-xs md:textsm">Most people don't have original ideas. Here is how Sam Altman pushes himself to have...</p>
+			</div>
 		</div>
 	</div>
 </div>
 
-<div class="w-[80%] mx-auto text-[14px]">
-	<div class="flex flex-row flex-wrap items-center gap-11 ">
+<!-- <div class="lg:w-[92%] xl:w-[83.3%] 2xl:w-[76.2%] my-0 mx-auto"> -->
+<div class="ml-[3.2rem] sm:ml-[3.3rem] md:ml-[0.1rem] lg:ml-[3.2rem] my-0 mx-auto">
+	<div class="flex gap-2 flex-row items-center">
+		<img src={forward} alt="forward line" class="w-6 block md:hidden">
+		<div class="flex flex-col w-[22%] sm:w-fit gap-1 md:text-right">
+			<img src={company} alt="company logo" class="w-6 md:w-9 md:ml-auto">
+			<h3 class="text-xs sm:text-sm md:text-base font-semibold md:font-bold">Duggup</h3>
+			<span class="text-[9px] md:text-xs text-[#7A9299]">San Francisco Bay Area</span>
+			<span class="text-[9px] md:text-xs text-[#7A9299]">Joined Nov 2023</span>
+		</div>
+		<img src={forward} alt="forward line" class="w-6 hidden md:block">
+		<div class="text-xs text-[#7A9299] w-fit ml-2 md:ml-14">
+			<h3 class="text-xs sm:text-sm md:text-xl font-semibold md:font-bold">Co-Founder and CEO</h3>
+			<h5 class="text-[9px] md:text-xs text-[#7A9299]">Full-time · Remote</h5>
+		</div>
+	</div>
+</div>
+
+<div class="w-[80%] 2xl:w-[70%] mx-auto text-[14px]">
+	<div class="flex flex-row items-center gap-11 ">
 		<div class="w-fit flex items-center">
 			<p class="text-xs text-[#7A9299]">Dec 2023</p>
 			<img src={progress} alt="progress line" class="w-fit">
 		</div>
-		<div class="w-[344px] border rounded-lg">
-			<img src={lessons} alt="startup lessons' thumbnail" class="w-fit">
-			<p class="px-8 py-4">Startup Lesson I am reflecting. Don't build for the "average person".</p>
-		</div>
-		<div class="w-[344px] border rounded-lg">
-			<img src={regrets} alt="life regrets' thumbnail" class="w-fit rounded-t-xl">
-			<p class="px-8 py-4">Your biggest regrets at 80 will be acts of omission.</p>
+		<div class="flex flex-wrap w-fit justify-center mx-auto sm:mt-9 gap-2 sm:gap-4 xl:gap-9">
+			<div class="w-[344px] border rounded-lg">
+				<img src={lessons} alt="startup lessons' thumbnail" class="w-fit">
+				<p class="px-1 md:px-8 py-1 sm:py-4 text-xs md:textsm">Startup Lesson I am reflecting. Don't build for the "average person".</p>
+			</div>
+			<div class="w-[344px] border rounded-lg">
+				<img src={regrets} alt="life regrets' thumbnail" class="w-fit rounded-t-xl">
+				<p class="px-1 sm:px-8 py-1 sm:py-4 text-xs md:textsm">Your biggest regrets at 80 will be acts of omission.</p>
+			</div>
 		</div>
 	</div>
 </div>
 
-<div class="w-[92%] my-0 mx-auto">
-	<div class="flex gap-2 flex-row flex-wrap items-center">
-		<div class="flex flex-col w-fit gap-1 text-right">
-			<img src={betterup} alt="company logo" class="w-9 ml-auto">
-			<h3 class="text-base font-bold">BetterUp</h3>
-			<span class="text-xs text-[#7A9299]">San Francisco Bay Area</span>
-			<span class="text-xs text-[#7A9299]">Joined
+<div class="ml-[3.2rem] sm:ml-[3.3rem] md:ml-[0.1rem] lg:ml-[3.2rem] my-0 mx-auto">
+	<div class="flex gap-2 flex-row items-center">
+		<img src={forward} alt="forward line" class="w-6 block md:hidden">
+		<div class="flex flex-col w-[22%] sm:w-fit gap-1 md:text-right">
+			<img src={betterup} alt="company logo" class="w-6 md:w-9 md:ml-auto">
+			<h3 class="text-xs sm:text-sm md:text-base font-semibold md:font-bold">BetterUp</h3>
+			<span class="text-[9px] md:text-xs text-[#7A9299]">San Francisco Bay Area</span>
+			<span class="text-[9px] md:text-xs text-[#7A9299]">Joined
 				Sep 2022</span>
 		</div>
-		<img src={forward} alt="forward line" class="w-6">
-		<div class="text-xs text-[#7A9299] w-fit ml-14">
-			<h3 class="text-xl font-bold">VP Engineering</h3>
-			<h5 class="text-xs text-[#7A9299]">Full-time</h5>
+		<img src={forward} alt="forward line" class="w-6 hidden md:block">
+		<div class="text-xs text-[#7A9299] w-fit ml-2 sm:ml-14">
+			<h3 class="text-xs sm:text-sm md:text-xl font-semibold md:font-bold">VP Engineering</h3>
+			<h5 class="text-[9px] md:text-xs text-[#7A9299]">Full-time</h5>
 		</div>
 	</div>
 </div>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -8,49 +8,63 @@
 	import down from '$lib/images/down.png';
 	import logout from '$lib/images/logout.png';
 	import settings from '$lib/images/settings.png';
+	import menu from '$lib/images/menu.png';
+	import close from '$lib/images/close.png';
 
 	let options = false;
 </script>
 
-<header class="sticky top-0 z-50 flex h-16 w-full items-center px-[16px] border-b border-[#525C69]/30 border-dotted backdrop-blur">
-	<div class="text-xl">
+<header class="sticky top-0 z-50 h-16 w-full flex justify-between items-center px-4 border-b border-[#525C69]/30 border-dotted backdrop-blur">
+	<span role="button" tabindex="0" class="block md:hidden w-fit" on:click={() => options=(!options)} on:keydown={(e)=> {if(e.key === "Enter" || e.key === " "){options=(!options);}}}>
+		{#if (options)}
+		<img src={close} alt="close menu icon" class="w-7">		
+		{:else}
+		<img src={menu} alt="hamburger menu icon" class="w-7">
+		{/if}
+	</span>
+
+	<div class="text-xl w-fit">
 		<a href="/" class="flex justify-start gap-2 w-fit">
-			<img src={logo} alt="Duggup Logo" class="w-fit"/>
+			<img src={logo} alt="Duggup Logo" class="w-fit md:w-11"/>
 			<h2 class="text-[#020817] font-semibold">duggup</h2>
 		</a>
 	</div>
 
-	<nav class="flex items-center text-sm font-[400] text-[#020817]">
-		<div class="flex">
-			<span class="min-w-[102px] text-center px-4 py-2 rounded-[4px] border-white border border-b-[4px] flex items-center justify-center outline-none gap-2 hover:cursor-pointer">
+	<nav class="flex max-w-fit items-center text-sm font-[400] text-[#020817]">
+		<div class="hidden md:flex">
+			<span class="w-[110px] text-center px-2 rounded-[4px] border-transparent border border-b-[4px] flex items-center justify-center outline-none gap-2 hover:cursor-pointer">
 				<img src={home} alt="home icon" class="w-fit">
 				Home
 			</span>
-			<span class="min-w-[102px] text-center px-4 py-2 rounded-[4px] border-white border border-b-[4px] flex items-center justify-center outline-none gap-2 hover:cursor-pointer">
+			<span class="w-[110px] text-center px-2 py-2 rounded-[4px] border-transparent border border-b-[4px] flex items-center justify-center outline-none gap-2 hover:cursor-pointer">
 				<img src={explore} alt="explore icon" class="w-fit">
 				Explore
 			</span>
-			<span class="min-w-[102px] text-center px-4 py-2 rounded-[4px] border-white border border-b-[4px] flex items-center justify-center outline-none gap-2 hover:cursor-pointer">
+			<span class="w-[110px] text-center px-2 py-2 rounded-[4px] border-transparent border border-b-[4px] flex items-center justify-center outline-none gap-2 hover:cursor-pointer">
 				<img src={learn} alt="learn icon" class="w-fit">
 				Learn
 			</span>
-			<span class="min-w-[102px] px-4 rounded-[4px] mx-5 border border-b-[4px] border-[#4D4D4D] flex items-center justify-center outline-none gap-2 hover:cursor-pointer">
+			<span class="max-w-[102px] px-4 rounded-[4px] mx-5 border border-b-[4px] border-[#4D4D4D] flex items-center justify-center outline-none gap-2 hover:cursor-pointer">
 				<img src={post} alt="post icon" class="w-fit">
 				Post<span class="w-1"></span>
 			</span>
 		</div>
-		<div class="w-[272px] ml-2">
+		<div class="min-w-fit max-w-[202px] ml-2">
 			<div role="button" tabindex="0" class="text-center z-[100] flex items-center justify-center cursor-pointer gap-[8px]" on:click={() => options=(!options)} on:keydown={(e)=> {if(e.key === "Enter" || e.key === " "){options=(!options);}}}>
-				<img src={profile} alt="user profile" class="w-9 p-[0.5px] border-[0.7px] rounded-full border-[#4d4d4d]">
-				<h3 class="font-[500] text-lg">Krishna Kiran</h3>
-				<img src={down} alt="down arrow icon" class="w-fit">
+				<img src={profile} alt="user profile" class="w-fit md:w-12 p-[0.5px] border-[0.7px] rounded-full border-[#4d4d4d]">
+				<h3 class="font-[500] text-lg hidden sm:block">Krishna Kiran</h3>
+				{#if (!options)}
+				<img src={down} alt="down arrow icon" class="w-6 hidden md:block">
+				{:else}
+				<img src={down} alt="down arrow icon" class="w-6 hidden md:block rotate-180">
+				{/if}
 			</div>
 		</div>
 	</nav>
 
 </header>
 {#if (options)}
-	<div class="options absolute z-[100] w-[212px] top-[70px] shadow-md h-24 pt-[5px] border rounded right-2">
+	<div class="hidden md:block options absolute z-[100] w-[212px] top-[70px] shadow-md h-24 pt-[5px] border rounded right-2">
 		<span class="w-[95%] mx-auto text-center px-1 py-2 rounded-[4px] hover:bg-[#ebf3fa] flex items-center justify-center outline-none gap-2 hover:cursor-pointer">
 			<img src={settings} alt="settings icon" class="w-5 opacity-60">
 			Settings
@@ -62,14 +76,50 @@
 	</div>
 {/if}
 
+{#if (options)}
+<div class="navbar flex md:hidden flex-col justify-center items-center gap-3 z-[100] bg-white h-svh w-screen absolute">
+	<span class="w-[110px] text-center px-2 py-2 rounded-[4px] border-transparent border border-b-[4px] flex items-center justify-start bg-inherit outline-none gap-2 hover:cursor-pointer">
+		<img src={home} alt="home icon" class="w-7 bg-inherit">
+		Home
+	</span>
+	<span class="w-[110px] text-center px-2 py-2 rounded-[4px] border-transparent border border-b-[4px] flex items-center justify-start bg-inherit outline-none gap-2 hover:cursor-pointer">
+		<img src={explore} alt="explore icon" class="w-7 bg-inherit">
+		Explore
+	</span>
+	<span class="w-[110px] text-center px-2 py-2 rounded-[4px] border-white border border-b-[4px] flex items-center justify-start outline-none gap-2 hover:cursor-pointer bg-inherit">
+		<img src={learn} alt="learn icon" class="w-7 bg-inherit">
+		Learn
+	</span>
+	<span class="w-[110px] text-center px-2 py-2 rounded-[4px] border-transparent border border-b-[4px] flex items-center justify-start outline-none gap-2 hover:cursor-pointer bg-inherit">
+		<img src={post} alt="post icon" class="w-7 bg-inherit">
+		Post<span class="w-1"></span>
+	</span>
+	<span class="w-[110px] text-center px-2 py-2 rounded-[4px] flex items-center justify-start outline-none gap-2 hover:cursor-pointer bg-inherit">
+		<img src={settings} alt="settings icon" class="w-[20%] opacity-60 bg-inherit">
+		Settings
+	</span>
+	<span class="w-[110px] text-center px-2 py-2 rounded-[4px] flex items-center justify-start outline-none gap-2 hover:cursor-pointer bg-inherit">
+		<img src={logout} alt="logout icon" class="w-[20%] opacity-80 bg-inherit">
+		Logout
+	</span>
+</div>
+{/if}
 
 <style>
 	.options{
-		animation: op1 0.6s;
+		animation: op 0.6s;
 	}
-	@keyframes op1{
+	@keyframes op{
 		0%{transform: translateY(-55px); opacity: 0;}
 		50%{opacity: 0;}
 		100%{transform: translateY(0); opacity: 1;}
+	}
+
+	.navbar{
+		animation: nav 1s;
+	}
+	@keyframes nav{
+		0%{transform: translateX(-555px);}
+		100%{transform: translateX(0);}
 	}
 </style>
